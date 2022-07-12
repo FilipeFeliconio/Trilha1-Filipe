6f6ac6af91b4c4655fa7e73b8e124fa1a4c4e250
Filipe Feliconio <filipe.feliconio@gmail.com>
2022-07-12 13:41:55 -0300

    add Exercício React Hello World

---
+++ Exercício React HelloWorld/react-hello/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import FirstHello from './components/FirstHello';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Hello World</h1>
+       <FirstHello />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Exercício React HelloWorld/react-hello/src/components/FirstHello.js
@@ -0,0 +1,12 @@
+function FirstHello() {
+    
+    //Primeiro Hello World
+    
+    return(
+        <div className="firsthello">
+            <p>Hello World</p>
+        </div>
+    )
+}
+
+export default FirstHello
